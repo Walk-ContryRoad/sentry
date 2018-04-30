9dc0d8bd46912602f0f8a35620b3d4a7ffe4a085
Lauryn Brown <lauryndbrown@gmail.com>
2018-04-30 12:30:21 -0700

    Added a flag and a place to add other internal plugins.

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1239,6 +1239,10 @@ SENTRY_DEFAULT_INTEGRATIONS = (
     'sentry.integrations.vsts.VSTSIntegration',
 )
 
+SENTRY_INTERNAL_INTEGRATIONS = (
+    'vsts',
+)
+
 
 def get_raven_config():
     return {
